abdd576756539c1ba385265b65c9050ccc42e13a
Alistair Keiller <alistair@keiller.net>
2022-09-19 15:50:24 -0700

    test

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 set -e
 
 # install emscripten
@@ -12,3 +11,8 @@ cd ..
 # download sysroot
 wget -qO- https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-16/wasi-sysroot-16.0.tar.gz | tar -xz
 wget -qO- https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-16/libclang_rt.builtins-wasm32-wasi-16.0.tar.gz | tar -xz
+
+# build
+emcmake cmake -G Ninja -S . -B build \
+    -DCMAKE_BUILD_TYPE=Release
+ninja -C build
