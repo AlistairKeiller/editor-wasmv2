3a26c65a252ef66525f4b7936ef6b37a75628707
Alistair Keiller <alistair@keiller.net>
2022-09-26 14:06:24 -0700

    improve main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,7 @@
 #include "clang/CodeGen/CodeGenAction.h"
 #include "llvm/Support/TargetSelect.h"
 
-using namespace clang;
-
-// std::unique_ptr<CompilerInstance> Clang;
-std::unique_ptr<EmitObjAction> Obj;
+clang::EmitObjAction Obj;
 
 extern "C" void init()
 {
@@ -13,27 +10,25 @@ extern "C" void init()
     llvm::InitializeAllTargetMCs();
     llvm::InitializeAllAsmPrinters();
     llvm::InitializeAllAsmParsers();
-
-    Obj = std::unique_ptr<EmitObjAction>(new EmitObjAction());
-
-    // Clang = std::unique_ptr<CompilerInstance>(new CompilerInstance());
-    // Clang->createDiagnostics();
-    // Clang->getHeaderSearchOpts().AddPath("include/clang", frontend::System, false, true);
-    // Clang->getFrontendOpts().Inputs.push_back(FrontendInputFile("main.cpp", Language::CXX));
-    // Clang->getFrontendOpts().OutputFile = "main.obj";
-    // Clang->getTargetOpts().Triple = "wasm32-wasi";
 }
 
 extern "C" int compile()
 {
-    // I dont understand why we need to create a new CompilerInstance every call
-    std::unique_ptr<CompilerInstance> Clang(new CompilerInstance());
-    Clang->createDiagnostics();
-    Clang->getHeaderSearchOpts().AddPath("sysroot/include", frontend::System, false, true);
-    Clang->getHeaderSearchOpts().AddPath("sysroot/include/c++/v1", frontend::System, false, true);
-    Clang->getHeaderSearchOpts().AddPath("llvm-project/llvm/lib/clang/16.0.0/include", frontend::System, false, true);
-    Clang->getFrontendOpts().Inputs.push_back(FrontendInputFile("main.cpp", Language::CXX));
-    Clang->getFrontendOpts().OutputFile = "main.obj";
-    Clang->getTargetOpts().Triple = "wasm32-wasi";
-    return Clang->ExecuteAction(*Obj);
+    clang::CompilerInstance Clang;
+
+    Clang.createDiagnostics();
+    Clang.getDiagnosticOpts().ShowColors = true;
+
+    Clang.getFrontendOpts().Inputs.emplace_back("main.cpp", clang::Language::CXX);
+    Clang.getFrontendOpts().OutputFile = "main.obj";
+
+    Clang.getHeaderSearchOpts().AddPath("sysroot/include", clang::frontend::System, false, true);
+    Clang.getHeaderSearchOpts().AddPath("sysroot/include/c++/v1", clang::frontend::System, false, true);
+    Clang.getHeaderSearchOpts().AddPath("llvm-project/llvm/lib/clang/16.0.0/include", clang::frontend::System, false, true);
+
+    // Clang->getPreprocessorOpts().addMacroDef("__wasi__");
+
+    Clang.getTargetOpts().Triple = "wasm32-wasi";
+
+    return Clang.ExecuteAction(Obj);
 }
